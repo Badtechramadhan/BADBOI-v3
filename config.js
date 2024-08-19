06d8ab630aa72016ea568766c188d13ff7221d57
Badtechramadhan <badtechramadhan@gmail.com>
2024-08-19 15:42:38 +0300

    Update config.js

--- config.js
+++ config.js
@@ -14,41 +14,41 @@ global.calender = d.toLocaleDateString('id')
 //===========================//
 
 //General Settings 
-global.apikey = 'ptla_6pKq2Q5hGxajeMBrRdazQLkG1NSpXwIIi1FYZNmkUkQ' //Ganti pake apikey panel lu
-global.capikey = 'ptlc_USy1wdkOexq5IIfootLXskwsgoZvDu1oQvgybJ0qlWC' //Ganti Pake Capikey Panel Lu
-global.domain = 'https://zetsubo-stardust.zxvision.biz.id'
+global.apikey = 'ptla_zJrHnQy9TNjr0ytG5qSeNuXKS0szz2wHUM9i2H2JDwp' //Ganti pake apikey panel lu
+global.capikey = 'ptla_zJrHnQy9TNjr0ytG5qSeNuXKS0szz2wHUM9i2H2JDwp' //Ganti Pake Capikey Panel Lu
+global.domain = 'https://skyzzhosting.hadzpediaa.biz.id'
 global.eggsnya = '15' //Ganti Pake Eggs Panel Lu
 global.location = '1' //Ganti Pake Location Panel Lu
 
 //===========================//
 
 global.prefa = ['','!','.',',','🐤','🗿']
-global.owner = ['2348140825959']
-global.ownMain = '2348140825959'
-global.NamaOwner = '𝓑𝓐𝓓𝓑𝓞𝓘' //gausah diganti 
-global.sessionName = 'badboisession'
+global.owner = ['255726998131']
+global.ownMain = '256726998131'
+global.NamaOwner = 'ℜ𝔄𝔐𝔄𝔇ℌ𝔄𝔑' //gausah diganti 
+global.sessionName = 'Ramadhansession'
 global.connect = true // Ubah Ke False Jika Ingin Menggunakan Qr Code
-global.namabot = '𝓑𝓐𝓓𝓑𝓞𝓘-𝐕3' //ganti aj klo mau
-global.author = '𝓑𝓐𝓓𝓑𝓞𝓘' //ganti aj klo mau
-global.packname = '𝓑𝓐𝓓𝓑𝓞𝓘 𝙱𝚘𝚝' //ganti aj klo mau
+global.namabot = '𝑹𝑨𝑴𝑨𝑫𝑯𝑨𝑵-𝑽3' //ganti aj klo mau
+global.author = '𝑹𝑨𝑴𝑨𝑫𝑯𝑨𝑵 𝑴𝑶𝑫𝒁' //ganti aj klo mau
+global.packname = '𝑹𝑨𝑴𝑨𝑫𝑯𝑨𝑵 𝙱𝚘𝚝' //ganti aj klo mau
 global.yt = 'https://whatsapp.com/channel/0029VadCyFZGufJ2YW4bG42x' //gausah diganti
 
 //===========================//
 
 global.listr = ` │⬡ 1GB Cpu: 50% - 3.000/bln
- │⬡ 2GB Cpu: 70% - 5.000/bln
- │⬡ 3GB Cpu: 90% - 7.000/bln
- │⬡ 4GB Cpu: 110% - 9.000/bln
- │⬡ 5GB Cpu: 130% - 11.000/bln
- │⬡ 6GB Cpu: 150% - 13.000/bln
- │⬡ 7GB Cpu: 170% - 15.000/bln
- │⬡ 8GB Cpu: 200% - 17.000/bln`
+ │⬡ 2GB Cpu: 70% - 1$
+ │⬡ 3GB Cpu: 90% - 1.5$
+ │⬡ 4GB Cpu: 110% - 1.7$
+ │⬡ 5GB Cpu: 130% - 1.9$
+ │⬡ 6GB Cpu: 150% - 2$
+ │⬡ 7GB Cpu: 170% - 2.5$
+ │⬡ 8GB Cpu: 200% - 3$`
  
 //===========================//
 
-global.country = `234`
+global.country = `256`
 global.system = {
-gmail: `vinorvanderly@gmail.com`,
+gmail: `badtechramadhan@gmail.com`,
 }
 
 //===========================//
@@ -145,14 +145,14 @@ else return emot[results[0][0]]
    //Respon
 global.mess = { // bagian ini gausah diganti 
 ingroup: '𝚃𝚑𝚒𝚜 𝚌𝚘𝚖𝚖𝚊𝚗𝚍 𝚌𝚊𝚗 𝚘𝚗𝚕𝚢 𝚋𝚎 𝚞𝚜𝚎𝚍 𝚘𝚗 𝚐𝚛𝚘𝚞𝚙𝚜💢',
-admin: `❗𝚂𝚘𝚛𝚛𝚢 𝚈𝚘𝚞 𝚊𝚛𝚎 𝚗𝚘𝚝 𝚘𝚗𝚎 𝚘𝚏 𝚝𝚑𝚎 𝚊𝚍𝚖𝚒𝚗𝚜 😝`,
-owner: `𝚈𝚘𝚞 𝚊𝚛𝚎 𝚗𝚘𝚝 \`꙲𝕭𝕬𝕯𝕭𝕺𝕴-𝓿3\` 𝚈𝚘𝚞 𝚌𝚊𝚗𝚝 𝚊𝚌𝚌𝚎𝚜𝚜🗣️`,
+admin: `❗𝚂𝚘𝚛𝚛𝚢 𝚈𝚘𝚞 𝚊𝚛𝚎 𝚗𝚘𝚝 𝚘𝚗𝚎 𝚘𝚏 𝚝𝚑𝚎 𝚊𝚍𝚖𝚒𝚗𝚜 fuck😝`,
+owner: `𝚈𝚘𝚞 𝚊𝚛𝚎 𝚗𝚘𝚝 \`𝑹𝑨𝑴𝑨𝑫𝑯𝑨𝑵-𝑽3\` 𝚈𝚘𝚞 𝚌𝚊𝚗𝚝 𝚊𝚌𝚌𝚎𝚜𝚜🗣️`,
 premium: '𝚈𝚘𝚞 𝚊𝚛𝚎 𝚗𝚘𝚝 𝚘𝚗𝚎 𝚘𝚏 𝚝𝚑𝚎 𝚙𝚛𝚎𝚖𝚒𝚞𝚖 𝚖𝚎𝚖𝚋𝚎𝚛𝚜 𝚜𝚘𝚛𝚛𝚢🐦',
 seller: 'Lu bukan seller, Jadi gabakal bisa make😹',
 usingsetpp: '𝙿𝚛𝚘𝚏𝚒𝚕𝚎 𝚙𝚒𝚌 𝚞𝚙𝚍𝚊𝚝𝚎𝚍 𝚜𝚞𝚌𝚌𝚎𝚜𝚏𝚞𝚕 🤓',
 wait: '*🕙𝚆𝚊𝚒𝚝 𝚊𝚜 𝙸 𝚙𝚛𝚘𝚌𝚌𝚎𝚜 𝚢𝚘𝚞𝚛 𝚛𝚎𝚚𝚞𝚎𝚜𝚝🗿*',
-succes: '🌆𝙳𝚘𝚗𝚎 𝚋𝚢 Dev 𝕭𝕬𝕯𝕭𝕺𝕴-𝓿3🗿🚬',
-bugrespon: `✅𝙿𝚛𝚘𝚌𝚌𝚎𝚜𝚒𝚗𝚐🕷️`
+succes: '🌆𝙳𝚘𝚗𝚎 𝚋𝚢 Dev 𝑹𝑨𝑴𝑨𝑫𝑯𝑨𝑵-𝑽3🗿🚬',
+bugrespon: `✅𝑭𝒖𝒄𝒌 𝒚𝒐𝒖 𝒊𝒕 𝒊𝒔 𝙿𝚛𝚘𝚌𝚌𝚎𝚜𝚒𝚗𝚐🕷️`
 }
 
 //===========================//
